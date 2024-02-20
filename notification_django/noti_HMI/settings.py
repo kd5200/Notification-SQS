94d98167ded601300e2dc2bfa0b80ce5e7a75e0a
kd5200 <kareemdavis18@yahoo.com>
2024-02-20 15:58:26 -0500

    project setup

---
+++ notification_django/noti_HMI/settings.py
@@ -0,0 +1,9 @@
+# settings.py
+# Environment variables will be set 
+
+AWS_ACCESS_KEY_ID = 'access_key_id'
+AWS_SECRET_ACCESS_KEY = 'secret_access_key'
+AWS_REGION_NAME = 'aws_region'
+
+# SQS configuration
+AWS_SQS_QUEUE_NAME = 'your_queue_name'
